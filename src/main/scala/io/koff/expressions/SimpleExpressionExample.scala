7fae7e41a241f6f9c5b5c4691606dda6e0d83238
Aleksei Shamenev <coffius@gmail.com>
2015-08-16 15:45:46 +0300

    add an example for auto extraction

--- src/main/scala/io/koff/expressions/SimpleExpressionExample.scala
+++ src/main/scala/io/koff/expressions/SimpleExpressionExample.scala
@@ -5,7 +5,6 @@ import com.github.jedesah.Expression._
 import scalaz._
 import Scalaz._
 import scalaz.std.scalaFuture.futureInstance
-import com.github.jedesah.Expression.auto.extract
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
@@ -14,44 +13,53 @@ import scala.concurrent.{Await, Future}
  * A very simple example which shows how to use Expression object
  */
 object SimpleExpressionExample extends App {
-
   /**
    * Async operation #1
    */
-  def future1(): Future[String] = {
-    Future.successful("future1")
-  }
-
+  def future1(): Future[String] = Future.successful("future1")
   /**
    * Async operation #2
    */
-  def future2(): Future[String] = {
-    Future.successful("future2")
-  }
-
+  def future2(): Future[String] = Future.successful("future2")
   /**
    * Result calculation
    */
-  def resultCalc(str1: String, str2: String): String = {
-    str1 + " | " + str2
-  }
-
+  def resultCalc(str1: String, str2: String): String = str1 + " | " + str2
   /**
    * We need to use `extract(...)` method in order to get result from futures
    */
-  val result = Expression[Future, String] {
+  def expression(): Future[String] = Expression[Future, String] {
     val result1 = extract(future1())
     val result2 = extract(future2())
     resultCalc(result1, result2)
   }
 
-  val forComprehensionAnalog = for {
+  /**
+   * For-Comprehension analog for `expression()` method
+   */
+  def forComprehension(): Future[String] = for {
     result1 <- future1()
     result2 <- future2()
   } yield {
       resultCalc(result1, result2)
   }
 
-  println("expression: " + Await.result(result, 30 seconds))
-  println("for-comprehension: " + Await.result(forComprehensionAnalog, 30 seconds))
+  println("expression: " + Await.result(expression(), 30 seconds))
+  println("for-comprehension: " + Await.result(forComprehension(), 30 seconds))
+
+  /**
+   * Example of usage of auto extraction
+   */
+  def autoExtract(): Future[String] = {
+    import com.github.jedesah.Expression.auto.extract
+    Expression[Future, String] {
+      val result1 = future1()
+      val result2 = future2()
+      resultCalc(result1, result2)
+    }
+  }
+
+  println("autoExtract: " + Await.result(autoExtract(), 30 seconds))
+
+  def expressionIfExample(): Future[String] =
 }
